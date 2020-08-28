f69f9510627d768043a90a267af2b2ccf133639a
AbdoSobhy <AbdoAhmedSobhy@hotmail.com>
2020-08-28 13:13:54 +0200

    first commit

---
+++ GoalLeaugeSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GoalLeaugeSwiftUI
+//
+//  Created by Abdelrahman Sobhy on 8/28/20.
+//  Copyright © 2020 Macbook pro. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Pods/Target Support Files/Pods-GoalLeaugeSwiftUI/Pods-GoalLeaugeSwiftUI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GoalLeaugeSwiftUI : NSObject
+@end
+@implementation PodsDummy_Pods_GoalLeaugeSwiftUI
+@end
